1eed42f18dfdf5aeb303c4ea2a4276be024610a9
hana147 <123791748+Hannicho1@users.noreply.github.com>
2023-04-28 22:57:34 -0400

    last commit

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -55,8 +55,17 @@ return carto;
 
   function markerPlace(array, map) {
     console.log('array for markers', array);
+
+    map.eachLayer((layer) => {
+        if (layer instanceof L.Marker) {
+          layer.remove();
+        }
+      });
+
     array.forEach((item) => {
         console.log('markerPlace', item);
+        const {coordinates} = item.geocoded_column_1;
+        L.marker([coordinates[1], coordinates[0]]).addTo(map);
 
     })
 
@@ -65,6 +74,7 @@ return carto;
     const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
     // const filterDataButton = document.querySelector('#filter');
     const loadDataButton = document.querySelector('#data_load');
+    const clearDataButton = document.querySelector('#data_clear');
     const generateListButton = document.querySelector('#generate');
     const textField = document.querySelector('#resto');
   
@@ -75,8 +85,8 @@ return carto;
     const carto = initMap();
 
     const storedData = localStorage.getItem("storedData");
-    const parsedData = JSON.parse(storedData);
-    if (parsedData.length > 0) {
+    let parsedData = JSON.parse(storedData);
+    if (parsedData?.length > 0) {
         generateListButton.classList.remove("hidden");
     }
 
@@ -114,8 +124,12 @@ return carto;
       // This changes the response from the GET into data we can use - an "object"
       const storedList = await results.json();
       localStorage.setItem('storedData', JSON.stringify(storedList));
-      // if (storedList.length > 0) {
-       // generateListButton.classList.remove('hidden');}
+      parsedData = storedList;
+
+       if (parsedData?.length > 0) {
+        generateListButton.classList.remove('hidden');
+    }
+
 
       loadAnimation.style.display = 'none';
       // console.table(storedList); 
@@ -139,6 +153,7 @@ return carto;
         currentList = cutRestaurantList(parsedData);
         console.log(currentList);
         injectHTML(currentList);
+        markerPlace(currentList, carto);
   
   
       });
@@ -148,7 +163,13 @@ return carto;
         const newList = filterList(currentList, event.target.value);
         console.log(newList);
         injectHTML(newList);
+        markerPlace(newList, carto);
       });
+
+      clearDataButton.addEventListener("click", (event) => {
+        console.log('clear browser data');
+        localStorage.clear('localStorage Check', localStorage.getItem("storeData"))
+      })
   
   
     /*
